ef61e4af811bbd4bb5ebf9d783c996dac00c1fc2
Benjamin Marquis <bmarquiscom@gmail.com>
2019-08-19 15:53:55 -0400

    Model validations and seeds implementation

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  has_many :items, dependent: :destroy
+  has_many :reservations_made_on_other_items, class_name: "Reservation"
+  has_many :reservations_on_own_items, through: :items, source: :reservations
 end
