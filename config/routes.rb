3539bc75b2687c3dea55fddf1e0761510311a2ba
Benjamin Marquis <bmarquiscom@gmail.com>
2019-08-19 16:28:05 -0400

    Routes implementation completed

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,21 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  # User routes
+  get "user/:id", to: "users#show"
+  # get "user/:id/reservations", to: "user#reservation"
+
+  # Item routes
+  get "item/new", to: "items#new"
+  post "item", to: "items#create"
+  patch "item/:item_id", to: "items#update"
+  delete "item/:item_id", to: "items#destroy"
+
+  # Reservation routes
+  post "item/:item_id/reservation", to: "reservations#create"
+  patch "reservation/reservation_id", to: "reservations#update"
+  get "reservation/reservation_id", to: "reservations#show"
+  delete "reservation/reservation_id", to: "reservations#destroy"
+
 end
