62a332b94df5baf272ef5d201a517fa0d44c4126
Benjamin Marquis <bmarquiscom@gmail.com>
2019-08-22 12:37:58 -0400

    Fisrt step map implementation

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,13 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!
   def home
     @items = Item.includes(:user)
+    @geocoded = @items.filter { |item| item.user.longitude && item.user.latitude }
+    @markers = @geocoded.map do |item|
+      {
+        lat: item.user.latitude,
+        lng: item.user.longitude
+      }
+    end
     @categories = Category.all
   end
 end
