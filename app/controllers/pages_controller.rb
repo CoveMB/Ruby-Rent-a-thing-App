d307f882124f681ab0efa54f870514ae413a88a0
Beau Ruster <rustey21@gmail.com>
2019-08-22 15:54:01 -0400

    search bar

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,13 @@
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!
   def home
-    @items = Item.all
-    @categories = Category.all
+    if params['search'].present?
+      @items = Item.where("title ILIKE ?", "%#{params['search']}%")
+      @categories = Category.joins(:items).where(items: {id:@items}).distinct
+    else
+      @items = Item.all
+      @categories = Category.all
+    end
+    @string = "You searched for #{params['search']}"
   end
 end
