5be1ca34bd48669ef012588a7ce24779ca3a8f5c
Benjamin Marquis <bmarquiscom@gmail.com>
2019-08-22 15:40:02 -0400

    Geocoding

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,10 +3,16 @@ class PagesController < ApplicationController
   def home
     @items = Item.includes(:user)
     @geocoded = @items.filter { |item| item.user.longitude && item.user.latitude }
+    p "************************************"
+    p @geocoded
     @markers = @geocoded.map do |item|
+      p "&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&"
+      p item
+      p item.user
       {
         lat: item.user.latitude,
-        lng: item.user.longitude
+        lng: item.user.longitude,
+        infoWindow: render_to_string(partial: "items/info_window", locals: { item: item })
       }
     end
     @categories = Category.all
--- app/javascript/packs/plugins/mapbox.js
+++ app/javascript/packs/plugins/mapbox.js
@@ -1,14 +1,38 @@
 import mapboxgl from 'mapbox-gl';
 
-const initMapbox = () => {
-  const mapElement = document.getElementById('map');
+const mapElement = document.getElementById('map');
 
-  if (mapElement) { // only build a map if there's a div#map to inject into
-    mapboxgl.accessToken = mapElement.dataset.mapboxApiKey;
-    const map = new mapboxgl.Map({
-      container: 'map',
-      style: 'mapbox://styles/mapbox/streets-v10'
-    });
+const buildMap = () => {
+  mapboxgl.accessToken = mapElement.dataset.mapboxApiKey;
+  return new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mapbox/streets-v10'
+  });
+};
+
+const addMarkersToMap = (map, markers) => {
+  markers.forEach((marker) => {
+    console.log(marker);
+    const popup = new mapboxgl.Popup().setHTML(marker.infoWindow);
+    new mapboxgl.Marker()
+      .setLngLat([ marker.lng, marker.lat ])
+      .setPopup(popup)
+      .addTo(map);
+  });
+};
+
+const fitMapToMarkers = (map, markers) => {
+  const bounds = new mapboxgl.LngLatBounds();
+  markers.forEach(marker => bounds.extend([ marker.lng, marker.lat ]));
+  map.fitBounds(bounds, { padding: 70, maxZoom: 10 });
+};
+
+const initMapbox = () => {
+  if (mapElement) {
+    const map = buildMap();
+    const markers = JSON.parse(mapElement.dataset.markers);
+    addMarkersToMap(map, markers);
+    fitMapToMarkers(map, markers);
   }
 };
 
