46f594e844fd06d0623c1526c9bdc7ef84df54d2
Beau Ruster <rustey21@gmail.com>
2019-08-23 10:29:14 -0400

    fixed-js

--- app/javascript/packs/plugins/filter.js
+++ app/javascript/packs/plugins/filter.js
@@ -1,29 +1,31 @@
-
 const initFilter = () => {
-  document.querySelectorAll('.category-checkbox').forEach((category) =>{
-    category.addEventListener('click',() => {
-      let count = 0;
-      console.log('clicked');
-
-     const cat = Array.from(document.querySelectorAll('.category-checkbox:checked')).map((selected) =>{
-        console.log(selected.dataset.category);
-        return selected.dataset.category;
-      });
-      document.querySelectorAll('.item').forEach((item) => {
-        item.style.display = 'none';
-        const found = cat.find((selected) => {
-          return (item.classList.contains(`category-${selected}`));
-        });
-        if (cat.length === 0 || found !== undefined){
-          count += 1;
-          item.style.display = 'block';
-        }
-
-      });
-      console.log(count);
-      document.getElementById("counter").innerHTML = `${count}`;
-    });
-  });
+ //adds event listener for each checkbox
+ document.querySelectorAll('.category-checkbox').forEach((category) =>{
+   category.addEventListener('click',() => {
+     let count = 0;
+     console.log('clicked');
+   //gets an array containing each selected category
+    const cat = Array.from(document.querySelectorAll('.category-checkbox:checked')).map((selected) =>{
+       console.log(selected.dataset.category);
+       return selected.dataset.category;
+     });
+    //iterates over each item
+     document.querySelectorAll('.item').forEach((item) => {
+       //default is to set item to hidden
+       item.style.display = 'none';
+       //checks if item is found in category array
+       const found = cat.find((selected) => {
+         return (item.classList.contains(`category-${selected}`));
+       });
+       if (cat.length === 0 || found !== undefined){
+         count += 1;
+         item.style.display = 'block';
+       }
+     });
+     console.log(count);
+     document.getElementById("counter").innerHTML = `${count}`;
+   });
+ });
 };
 
 export { initFilter };
