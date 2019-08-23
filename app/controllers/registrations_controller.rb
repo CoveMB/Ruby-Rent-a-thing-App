92ae694b0ad7d82fa523dd271f0cf83a3c4affe5
Benjamin Marquis <bmarquiscom@gmail.com>
2019-08-23 12:54:12 -0400

    Ajax reservation

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -4,4 +4,8 @@ class RegistrationsController < Devise::RegistrationsController
   def after_update_path_for(resource)
     user_path(resource)
   end
+
+  def update_resource(resource, params)
+    resource.update_without_password(params)
+  end
 end
