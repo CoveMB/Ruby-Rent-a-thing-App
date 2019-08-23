4be3cfa0471aa058b68f46ab0d70a1c80a4ff1f1
Benjamin Marquis <bmarquiscom@gmail.com>
2019-08-23 15:17:45 -0400

    Ad client side restriction

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -30,6 +30,8 @@ Rails.application.configure do
     config.cache_store = :null_store
   end
 
+  config.serve_static_assets = true
+  config.assets.compress = true
   # Store uploaded files on the local file system (see config/storage.yml for options)
   config.active_storage.service = :amazon
 
